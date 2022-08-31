5990cf3d43ee2b78305723db802bb6e6d564df68
Pedro Rondelli <pedrorondelli@hotmail.com>
2022-08-31 10:11:27 -0300

    Segundo Commit importação axios e fonte

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Estou aquui")
+requisicaoteste()
+
+function printar(resposta) {
+    console.log(resposta)
+}
+
+function requisicaoteste() {
+    let promessaTeste = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes")
+    promessaTeste.then(printar)
+    promessaTeste.catch(requisicaoteste)
+}
